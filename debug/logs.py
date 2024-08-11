2f589c1aea96c847219e2eb4fcd6aea11986d721
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2024-08-11 14:01:04 +0100

    Added write_to_log, and added crash reporter file

---
+++ debug/logs.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+def write_to_log(severity, msg):
+    time = str(datetime.utcnow())
+    with open("./debug/log.txt", "a") as log:
+        log.write(f"[{severity}] - {msg} - {time.split('.')[0]}\n")
