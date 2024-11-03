646b292475f657199359b38953a607fe95b3530e
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2024-11-03 21:47:18 +0000

    Added button_on_clicks

--- ui/main_menu/button_on_clicks.py
+++ ui/main_menu/button_on_clicks.py
@@ -10,6 +10,14 @@ def settings():
     return "settings"
 
 
+def single_player():
+    return "singleplayer"
+
+
+def multi_player():
+    return "multiplayer"
+
+
 def test_level():
     return "level", -999
 
@@ -18,5 +26,7 @@ button_on_clicks = {
     "Back": back,
     "Play": play,
     "Settings": settings,
-    "Test Level": test_level
+    "Test Level": test_level,
+    "Single Player": single_player,
+    "Multi Player": multi_player
 }
