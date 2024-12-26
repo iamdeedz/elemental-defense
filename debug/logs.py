ecbac33abaff211efcd79c56e6af8a37dcc87002
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2024-12-26 19:51:26 +0000

    Changed to non-deprecated time method

--- debug/logs.py
+++ debug/logs.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 
 def write_to_log(severity, msg):
-    time = str(datetime.utcnow())
+    time = str(datetime.now(UTC))
     with open("./elemental-defense-log.txt", "a") as log:
         log.write(f"[{severity}] - {msg} - {time.split('.')[0]}\n")
