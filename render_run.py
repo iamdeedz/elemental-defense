f793102443eac428ce6890f7b8f059938f4cfa70
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-07-11 19:07:17 +0100

    separated args in subprocess.run

--- render_run.py
+++ render_run.py
@@ -3,9 +3,9 @@ from subprocess import run
 
 
 def run_http():
-    run("python -m http.server 10000")
+    run(["python", "-m", "http.server", "10000"])
 
 
 if __name__ == "__main__":
     Process(target=run_http).start()
-    run("python server.py")
+    run(["python", "server.py"])
