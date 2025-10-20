bba4e098729279dae4cbe4138ad59ab6f4354063
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-10-20 18:10:24 +0100

    shop

--- ui/shop/shop.py
+++ ui/shop/shop.py
@@ -15,6 +15,7 @@ from pygame.font import Font
 
 class Shop:
     def __init__(self):
+        set_error_code("1901")
         self.windows = [ShopWindow(name) for name in ["closed", "selection"] + elements]
         self.window_keys = {window.name: i for i, window in enumerate(self.windows)}
         self.current_window = self.windows[0]
@@ -24,8 +25,10 @@ class Shop:
         self.element_being_placed = None
 
         self.range_circle = None
+        reset_error_code()
 
     def draw(self, screen):
+        set_error_code("1902")
         self.toggle_button.draw(screen)
         self.current_window.draw(screen)
 
@@ -34,7 +37,10 @@ class Shop:
             circle(surface, (0, 0, 0, 50), get_mouse_pos(), self.range_circle)
             screen.blit(surface, (0, 0))
 
+        reset_error_code()
+
     def update(self, towers, balance):
+        set_error_code("1903")
         should_tower_place = True
 
         # Check if any of tower is clicked (indicating the player wants to upgrade a tower)
@@ -71,13 +77,16 @@ class Shop:
             balance = self.place_tower(towers, balance, self.tower_being_placed)
             self.tower_being_placed = self.range_circle = None
 
+        reset_error_code()
         return balance
 
     def place_tower(self, towers, balance, tower):
+        set_error_code("1904")
         mouse_pos = get_mouse_pos()
         tower = all_towers[tower](mouse_pos)
         towers.append(tower)
         balance -= tower_costs[tower.name]
+        reset_error_code()
         return balance
 
 
